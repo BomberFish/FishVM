3d14602d1e97619604e80e1224f0b8a77190a837
BomberFish <bomberfish@bomberfish.ca>
2024-10-19 13:34:43 -0400

    Initial Commit

---
+++ FishVM/FishVMApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// FishVMApp.swift – FishVM
+// created on 2024-10-19
+
+import SwiftUI
+
+@main
+struct FishVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishVMTests/FishVMTests.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// FishVMTests.swift – FishVMTests
+// created on 2024-10-19
+
+import Testing
+@testable import FishVM
+
+struct FishVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
